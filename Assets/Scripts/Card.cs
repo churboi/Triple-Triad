383d23fcd4d3d184490b5ac5389bf446c055421e
v-a-p-o-r <36640074+v-a-p-o-r@users.noreply.github.com>
2018-04-16 11:01:49 -0700

    Start of project
    board made with highlight hovering

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+
+    public string cardName;
+    public int cardLevel;
+    public int[] cardStats;
+    public string element;
+
+    enum Direction { Top, Down, Left, Right };
+
+}
---
+++ Assets/Scripts/CardSlot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSlot : MonoBehaviour {
+
+    public Material[] slotMats;
+
+    private bool hasCard = false;
+    private string slotElement;
+    private bool isHighlighted = false;
+    private bool isMouseOn = false;
+    private Renderer thisRend;
+
+    private void Start()
+    {
+        thisRend = GetComponent<Renderer>();
+    }
+
+    private void OnMouseOver()
+    {
+        isHighlighted = true;
+        SlotHighlight(isHighlighted);
+    }
+    private void OnMouseExit()
+    {
+        isHighlighted = false;
+        SlotHighlight(isHighlighted);
+    }
+
+    private void SlotHighlight(bool isHovered)
+    {
+        if(isHovered && (!isMouseOn))
+        {
+            thisRend.material = slotMats[1];
+            isMouseOn = true;
+        }
+        else if((!isHovered))
+        {
+            thisRend.material = slotMats[0];
+            isMouseOn = false;
+        }
+    }
+}
